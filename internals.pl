265d0aeb5fa431b7f64185d7af9830d2a8ec9fe2
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2013-04-24 14:24:50 +0100

    m

---
+++ internals.pl
@@ -0,0 +1,70 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/section:streaming/;
+$ref_files{$key} = "$dir".q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/practica:chat_con_frames/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:websockets/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_metaprogramming/;
+$ref_files{$key} = "$dir".q|node98.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:correo/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chapter:activerecord/;
+$ref_files{$key} = "$dir".q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sectionpractica:abreviarurl/;
+$ref_files{$key} = "$dir".q|node45.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:requestobject/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:sinatrachat/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsectionpractica:chat/;
+$ref_files{$key} = "$dir".q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:broadcaststreaming/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:requestobject/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:authentication/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_aaltomwad/;
+$ref_files{$key} = "$dir".q|node98.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:basicauthentication/;
+$ref_files{$key} = "$dir".q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/practica:chat_con_mensajes_uno_uno/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ labels.pl
@@ -0,0 +1,133 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/section:streaming/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/practica:chat_con_frames/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:websockets/;
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_metaprogramming/;
+$external_labels{$key} = "$URL/" . q|node98.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:correo/;
+$external_labels{$key} = "$URL/" . q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chapter:activerecord/;
+$external_labels{$key} = "$URL/" . q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sectionpractica:abreviarurl/;
+$external_labels{$key} = "$URL/" . q|node45.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:requestobject/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:sinatrachat/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsectionpractica:chat/;
+$external_labels{$key} = "$URL/" . q|node23.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:broadcaststreaming/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:requestobject/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:authentication/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_aaltomwad/;
+$external_labels{$key} = "$URL/" . q|node98.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:basicauthentication/;
+$external_labels{$key} = "$URL/" . q|node29.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/practica:chat_con_mensajes_uno_uno/;
+$external_labels{$key} = "$URL/" . q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2008 (1.71)
+# labels from external_latex_labels array.
+
+
+$key = q/figure:broadcaststreaming/;
+$external_latex_labels{$key} = q|2.9.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:streaming/;
+$external_latex_labels{$key} = q|2.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/practica:chat_con_frames/;
+$external_latex_labels{$key} = q|2.9.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:requestobject/;
+$external_latex_labels{$key} = q|1.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:websockets/;
+$external_latex_labels{$key} = q|2.9.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:authentication/;
+$external_latex_labels{$key} = q|1.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:correo/;
+$external_latex_labels{$key} = q|2.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/chapter:activerecord/;
+$external_latex_labels{$key} = q|8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sectionpractica:abreviarurl/;
+$external_latex_labels{$key} = q|8.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:requestobject/;
+$external_latex_labels{$key} = q|1.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/figure:sinatrachat/;
+$external_latex_labels{$key} = q|2.9.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/practica:chat_con_mensajes_uno_uno/;
+$external_latex_labels{$key} = q|2.9.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsectionpractica:chat/;
+$external_latex_labels{$key} = q|2.9.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/section:basicauthentication/;
+$external_latex_labels{$key} = q|2.11|; 
+$noresave{$key} = "$nosave";
+
+1;
+
